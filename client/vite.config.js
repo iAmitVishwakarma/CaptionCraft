19a2ff4b9be3b6b2fa5e921e289d6c5c6d7ca17c
iAmitVishwakarma <iam.itvishwakarma3@gmail.com>
2025-08-17 22:35:29 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+server: {
+    proxy: {
+      '/api': {
+        target: 'https://captioncraft-cx47.onrender.com/', // Replace with your deployed backend URL
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
