a436b37ee66ed7f93306dae951972ef9c0f61f53
iAmitVishwakarma <iam.itvishwakarma3@gmail.com>
2025-08-17 22:56:58 +0530

    Update auth.routes.js

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const { registerController, loginController } = require("../controller/auth.controller")
+const authMiddleware = require("../middleware/auth.middleware");
 const router  = express.Router()
 
 router.post('/register',registerController)
 router.post('/login',loginController)
+router.get('/check', authMiddleware, (req, res) => {
+    // If authMiddleware succeeds, the user is authenticated
+    res.status(200).json({ message: "Authenticated" });
+});
 
 module.exports = router
